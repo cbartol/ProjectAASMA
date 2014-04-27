9e695b98089cc8225cb1a7950724d823a7789e47
Hugo Lopes <huguinhoman@gmail.com>
2014-04-27 18:30:30 +0100

    Very stupid ReactiveAI which creates needles

--- ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/Reactive/ReactiveAI.cs
+++ ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/Reactive/ReactiveAI.cs
@@ -27,6 +27,28 @@ namespace AASMAHoshimi.Reactive
             {
                 this._nanoAI.Build(typeof(ReactiveExplorer),"E"+this._explorerNumber++);
             }
+            else if(!getAASMAFramework().overHoshimiPoint(this._nanoAI) || getAASMAFramework().overNeedle(this._nanoAI)) 
+            {
+                List<System.Drawing.Point> visibleHoshimiesList = getAASMAFramework().visibleHoshimies(this._nanoAI);
+                if(visibleHoshimiesList.Count != 0) {
+                    System.Drawing.Point nearestHoshimi = Utils.getNearestPoint(this._nanoAI.Location, visibleHoshimiesList);
+                    if (!getAASMAFramework().visibleEmptyNeedles(this._nanoAI).Contains(nearestHoshimi) &&
+                        !getAASMAFramework().visibleFullNeedles(this._nanoAI).Contains(nearestHoshimi))
+                    {
+                        this._nanoAI.MoveTo(nearestHoshimi);
+                        return;
+                    }
+                }
+                if (frontClear()) {
+                    this.MoveForward();
+                } else {
+                    this.RandomTurn();
+                }
+            }
+            else if (getAASMAFramework().overHoshimiPoint(this._nanoAI) && (!getAASMAFramework().overNeedle(this._nanoAI)))
+            {
+                this._nanoAI.Build(typeof(ReactiveNeedle), "N" + this._needleNumber++);
+            }
         }
 
         public override void receiveMessage(AASMAMessage msg)
