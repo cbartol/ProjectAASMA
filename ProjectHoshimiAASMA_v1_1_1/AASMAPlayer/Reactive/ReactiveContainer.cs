146505fef5ed0966caf313d721bcd25d8fd516e0
André Rodrigues <fury252@gmail.com>
2014-04-28 00:09:27 +0100

    Renamed class to match the others
    Also commented the code to understand well what are the priorities of
    the ReactiveCollector.

---
+++ ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/Reactive/ReactiveContainer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using AASMAHoshimi;
+using PH.Common;
+
+namespace AASMAHoshimi.Reactive
+{
+    [Characteristics(ContainerCapacity = 20, CollectTransfertSpeed = 5, Scan = 0, MaxDamage = 0, DefenseDistance = 0, Constitution = 20)]
+    public class ReactiveCollector : AASMAContainer
+    {
+        public override void DoActions()
+        {
+            List<Point> visibleAZN = getAASMAFramework().visibleAznPoints(this);
+            List<Point> visibleEmptyNeedle = getAASMAFramework().visibleEmptyNeedles(this);
+
+            if (Stock < ContainerCapacity && this.getAASMAFramework().overAZN(this))
+            {
+                // Collect AZN if still has capacity and if it's over an AZN Point
+                this.collectAZN();
+            }
+            else if (Stock > 0 && this.getAASMAFramework().overEmptyNeedle(this))
+            {
+                // Transfer AZN if still has AZN and if it's over a Needle
+                this.transferAZN();
+            }
+            else if (Stock < ContainerCapacity && visibleAZN.Count > 0)
+            {
+                // Move to a visible AZN Point if container is not full
+                Point target = visibleAZN[0];
+                this.MoveTo(target);
+            }
+            else if (Stock > 0 && visibleEmptyNeedle.Count > 0)
+            {
+                // Move to a visible "empty" needle if still has AZN Points
+                Point target = visibleEmptyNeedle[0];
+                this.MoveTo(target);
+            }
+            else if (frontClear())
+            {
+                // Move front with 80% chance
+                if (Utils.randomValue(100) < 80)
+                {
+                    this.MoveForward();
+                }
+                else
+                {
+                    this.RandomTurn();
+                }
+            }
+            else
+            {
+                // Change direction randomly because it can't move front
+                this.RandomTurn();
+            }
+        }
+
+        public override void receiveMessage(AASMAMessage msg)
+        {
+            getAASMAFramework().logData(this, "received message from " + msg.Sender + " : " + msg.Content);
+        }
+    }
+}
