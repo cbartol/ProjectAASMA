d851641d2ceb5309189b78ceba12ed8ac5656b89
André Rodrigues <fury252@gmail.com>
2014-05-22 15:03:14 +0100

    Cenas que já me estou a passar

--- ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/Deliberative/DeliberativeAI.cs
+++ ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/Deliberative/DeliberativeAI.cs
@@ -11,8 +11,9 @@ namespace AASMAHoshimi.Deliberative
 	{
 		private List<Point> viewedHoshimies;
 		private List<Point> createdNeedles;
-		private Action action;
-		private Point targetAction;
+		private List<Point> viewedEnemies;
+		private List<Action> plan;
+		private Intention intention;
 
 		public DeliberativeAI(NanoAI nano)
 		{
@@ -21,9 +22,53 @@ namespace AASMAHoshimi.Deliberative
 			this.createdNeedles = new List<Point>();
 		}
 
+		public override void onAgentCreated (Type agentType)
+		{
+			if (Type.Equals (typeof(DeliberativeNeedle))) 
+			{
+				// TODO verify if this is really the point
+				this.createdNeedles.Add (this._nanoAI.PointInfo);
+			}
+		}
+
 		public override void DoActions()
 		{
 			// get perceptions
+			updatePerceptions ();
+
+			if (plan == null) {
+				// get desires
+				Intention[] desires = Options ();
+
+				// get intention based on the desires and the previous intention
+				this.intention = Filter (desires, this.intention);
+
+				this.plan = Plan (this.intention);
+			}
+
+			if (plan.Count > 0) {
+				// Continue with the same plan
+
+				Action action = this.plan [0];
+				action.execute ();
+				this.plan.RemoveAt (0);
+
+				updatePerceptions ();
+				if (Reconsider ()) {
+					action.cancel ();
+
+					// get desires
+					Intention[] desires = Options ();
+
+					// get intention based on the desires and the previous intention
+					this.intention = Filter (desires, this.intention);
+
+					this.plan = Plan (this.intention);
+				}
+			}
+		}
+
+		private void updatePerceptions() {
 			// update list of viewed hoshimies
 			List<Point> visibleHoshimies = getAASMAFramework ().visibleHoshimies (this._nanoAI);
 			foreach (Point p in visibleHoshimies) {
@@ -32,20 +77,8 @@ namespace AASMAHoshimi.Deliberative
 				}
 			}
 
-			// get desires
-			Action[] desires = options();
-
-			// get intention based on the desires and the previous intention
-			Point newTarget;
-			Action newAction = filter (desires, this.action, out newTarget);
-
-			// stop previous action if it's different
-			if (newAction != this.action || newTarget != this.targetAction) {
-				stopCurrentAction ();
-			}
-
-			// execute the plan (action)
-			executeAction (newAction, newTarget);
+			// update list of viewed enemies
+			viewedEnemies = getAASMAFramework ().visiblePierres (this);
 		}
 
 		public override void receiveMessage(AASMAMessage msg)
@@ -53,41 +86,34 @@ namespace AASMAHoshimi.Deliberative
 			// Empty on purpose
 		}
 
-		private Action[] options () {
-			return (Action[]) Enum.GetValues(typeof(Action));
+		private static Intention[] Options () {
+			return (Intention[]) Enum.GetValues(typeof(Intention));
 		}
 
 		/**
 		 * Must go to old needles sometimes
 		 */
-		private Action filter(Action[] desires, Action prevIntention, out Point newTarget) {
-			newTarget = new Point();
-
+		private static Intention Filter(Intention[] desires, Intention prevIntention) {
 			if (getAASMAFramework ().visiblePierres (this._nanoAI).Count != 0) {
+				return Intention.FLEE;
+			}
+
+			if (getAASMAFramework ().protectorsAlive () < 10 ||
+			    getAASMAFramework ().containersAlive () < 10 ||
+				getAASMAFramework ().explorersAlive () < 10 ||
+				(getAASMAFramework ().overHoshimiPoint (this._nanoAI) && 
+					(!getAASMAFramework ().overNeedle (this._nanoAI))))
+			{
+				return Intention.CREATE_AGENT;
+			}
 				List<Point> possibleMoves = new List<Point> ();
 				foreach (Point enemy in getAASMAFramework().visiblePierres(this._nanoAI)) {
 					possibleMoves.Add (Utils.oppositDirection (this._nanoAI.Location, enemy, getAASMAFramework ().Tissue));
 				}
-				newTarget = Utils.getMiddlePoint (possibleMoves.ToArray ());
+				Point target = Utils.getMiddlePoint (possibleMoves.ToArray ());
 
 				return Action.FLEE;
-			} 
 
-			if (getAASMAFramework ().protectorsAlive () < 5) {
-				return Action.CREATE_AGENT_PROTECTOR; 
-			} 
-
-			if (getAASMAFramework ().containersAlive () < 5) {
-				return Action.CREATE_AGENT_CONTAINER;
-			} 
-
-			if (getAASMAFramework ().explorersAlive () < 5) {
-				return Action.CREATE_AGENT_EXPLORER;
-			}
-
-			if (getAASMAFramework ().overHoshimiPoint (this._nanoAI) && (!getAASMAFramework ().overNeedle (this._nanoAI))) {
-				return Action.CREATE_AGENT_NEEDLE;
-			}
 
 			if (!getAASMAFramework ().overHoshimiPoint (this._nanoAI) || getAASMAFramework ().overNeedle (this._nanoAI)) {
 				// TODO take into account the distance (choose the closest spot)
@@ -103,37 +129,42 @@ namespace AASMAHoshimi.Deliberative
 			return Action.MOVE_AROUND;
 		}
 
-		private void executeAction(Action action, Point targetAction) {
-			if (action == Action.MOVE_AROUND) {
-				this.MoveToClearPosition (90);
-			} else if (action == Action.MOVE_CREATED_HOSHIMIE || action == Action.FLEE || 
-				action == Action.MOVE_EMPTY_HOLE) {
-				this._nanoAI.MoveTo (targetAction);
-			} else if (action == Action.CREATE_AGENT_CONTAINER) {
-				this._nanoAI.Build (typeof(DeliberativeContainer), "C");
-			} else if (action == Action.CREATE_AGENT_EXPLORER) {
-				this._nanoAI.Build (typeof(DeliberativeExplorer), "E");
-			} else if (action == Action.CREATE_AGENT_NEEDLE) {
-				this.createdNeedles.Add (targetAction);
-				this._nanoAI.Build (typeof(DeliberativeNeedle), "N");
-			} else if (action == Action.CREATE_AGENT_PROTECTOR) {
-				this._nanoAI.Build (typeof(DeliberativeProtector), "P");
-			}
+//		private void executeAction(Action action, Point targetAction) {
+//			if (action == Action.MOVE_AROUND) {
+//				this.MoveToClearPosition (90);
+//			} else if (action == Action.MOVE_CREATED_HOSHIMIE || action == Action.FLEE || 
+//				action == Action.MOVE_EMPTY_HOLE) {
+//				this._nanoAI.MoveTo (targetAction);
+//			} else if (action == Action.CREATE_AGENT_CONTAINER) {
+//				this._nanoAI.Build (typeof(DeliberativeContainer), "C");
+//			} else if (action == Action.CREATE_AGENT_EXPLORER) {
+//				this._nanoAI.Build (typeof(DeliberativeExplorer), "E");
+//			} else if (action == Action.CREATE_AGENT_NEEDLE) {
+//				this.createdNeedles.Add (targetAction);
+//				this._nanoAI.Build (typeof(DeliberativeNeedle), "N");
+//			} else if (action == Action.CREATE_AGENT_PROTECTOR) {
+//				this._nanoAI.Build (typeof(DeliberativeProtector), "P");
+//			}
+//
+//			this.action = action;
+//			this.targetAction = targetAction;
+//		}
 
-			this.action = action;
-			this.targetAction = targetAction;
-		}
-
-		private void stopCurrentAction() {
-			if (this.action == Action.FLEE || this.action == Action.MOVE_AROUND ||
-				this.action == Action.MOVE_EMPTY_HOLE || this.action == Action.MOVE_CREATED_HOSHIMIE) {
-				this._nanoAI.StopMoving ();
+		private static List<Action> Plan(Intention intention) {
+			switch (intention) {
+				case Intention.CREATE_AGENT:
+					break;
+			
+			case Intention.
 			}
+		} 
+
+		private static bool Reconsider() {
+			// TODO
 		}
 	}
 
-	enum Action {
-		MOVE_AROUND, FLEE, MOVE_CREATED_HOSHIMIE, MOVE_EMPTY_HOLE,
-		CREATE_AGENT_CONTAINER, CREATE_AGENT_EXPLORER, CREATE_AGENT_NEEDLE, CREATE_AGENT_PROTECTOR
+	enum Intention {
+		MOVE_AROUND, FLEE, MOVE_CREATED_HOSHIMIE, CREATE_AGENT
 	}
 }
