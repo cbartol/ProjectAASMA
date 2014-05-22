6885c9656165dd196f3f4928ba722bca9c935ba3
André Rodrigues <fury252@gmail.com>
2014-05-22 14:45:45 +0100

    Added support to Actions

---
+++ ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/Action/MoveAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PH.Common;
+using System.Drawing;
+
+namespace AASMAHoshimi
+{
+	public class MoveAction : Action
+	{
+		private NanoMoveable owner;
+		private Point target;
+
+		public MoveAction (NanoMoveable owner, Point target)
+		{
+			this.owner = owner;
+			this.target = target;
+		}
+
+		public void execute() 
+		{
+			this.owner.MoveTo (target);
+		}
+
+		public void cancel() 
+		{
+			this.owner.StopMoving ();
+		}
+	}
+}
+
